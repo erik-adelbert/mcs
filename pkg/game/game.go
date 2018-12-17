6b4ed7422ea964a489a63b74d7dfcf90b6b5db11
erik-adelbert <42875866+erik-adelbert@users.noreply.github.com>
2018-12-17 21:55:28 +0100

    Add files via upload

---
+++ pkg/game/game.go
@@ -0,0 +1,7 @@
+// Copyright 2018 Erik Adelbert. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// game can be used to represent board games like clickomania or samegame.
+package game
+
