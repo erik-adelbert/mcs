a7c0e0453134b7865c73c233aa51ff4cfcb17cac
erika <erik@adelbert.fr>
2018-12-23 20:32:09 +0100

    pkg/game > pkg/chaingame

--- pkg/chaingame/game.go
+++ pkg/chaingame/game.go
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// game can be used to represent board games like clickomania or samegame.
+// chaingame can be used to represent board games like clickomania or samegame.
 package chaingame
